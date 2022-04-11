0ee0c8d6d660646d4e588f517a056bcde1db644c
Bram Jans <ik@bramjans.com>
2022-04-11 16:14:32 +0200

    Add updated transactions for synchronization endpoint

--- lib/ponto/api/transaction.rb
+++ lib/ponto/api/transaction.rb
@@ -1,9 +1,15 @@
 module Ponto
   class Transaction < Ponto::BaseResource
-    def self.list(account_id:, access_token: nil, headers: nil, **query_params)
-      uri = Ponto.api_schema["account"]["transactions"]
-        .sub("{accountId}", account_id)
-        .sub("{transactionId}", "")
+    def self.list(account_id: nil, synchronization_id: nil, access_token: nil, headers: nil, **query_params)
+      uri = if synchronization_id
+              Ponto.api_schema["synchronization"]["updatedTransactions"]
+                .sub("{synchronizationId}", synchronization_id)
+                .sub("{transactionId}", "")
+            else
+              Ponto.api_schema["account"]["transactions"]
+                .sub("{accountId}", account_id)
+                .sub("{transactionId}", "")
+            end
       list_by_uri(uri: uri, query_params: query_params, headers: headers, access_token: access_token)
     end
 
